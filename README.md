6c438addbd1088a3074798b0bc5e739f0a9a8674
Luis Moneda <lg.moneda@gmail.com>
2018-11-25 16:08:03 -0200

    Markov (#2)
    * partition
    
    * update md
    
    * md
    
    * change header
    
    * index
    
    * fix links

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # mac6916
 
-#### Python environment
+## Index
+
+1. [Python Environment](#python-environment)
+2. [Bayes Ball (L3Q4)](#bayes-ball)
+3. [Markov Partition (L4Q3)](#markov-partition)
+
+## Python environment
 
 Creating an environment from the `environment.yml` file:
 ```
 conda env create -f environment.yml
 ```
 
-#### Bayes Ball
+## Bayes Ball
 
 Just run `python bayes-ball/bayes_ball.py`.
 
@@ -30,3 +36,39 @@ check_dseparation(A, L, [E], network)
 ```
 
 It checks if A and L are d-separated given E.
+
+## Markov Partition
+
+Run:
+
+``` 1c-enterprise
+python markov/markov.py misconception.uai
+```
+
+The `misconception.uai` file comes from an example from this [book](### http://www.cs.cmu.edu/~epxing/Class/10708-13/reading/Ch%204.pdf). The partition result match the one in the book (page 105, figure 4.2 legend).
+
+The example given pic 2011] is on the `problem.uai` file.
+
+An example of bayesian network encoded by the undirected graph can be found on the `bayesian.uai` file:
+
+Structure: B <- A -> C
+
+| A        | P(A)           |
+| ------------- |:-------------:|
+| 0      | 0.4 |
+| 1      | 0.6      |
+
+
+| A             | B               | P(B \| A) | P(A, B)|
+| ------------- | :-------------: |:---:|:---:|
+| 0             | 0             | 0.4 | 0.16   |
+| 0             | 1             | 0.3 | 0.12   |
+| 1             | 0             | 0.3 | 0.36   |
+| 1             | 1             | 0.3 | 0.42   |
+
+| A             | C               | P(C \| A) | P(A, C)|
+| ------------- | :-------------: |:---:|:---:|
+| 0             | 0             | 0.1 | 0.04   |
+| 0             | 1             | 0.2 | 0.08   |
+| 1             | 0             | 0.9 | 0.24   |
+| 1             | 1             | 0.8 | 0.48   |
---
+++ markov/markov.py
@@ -0,0 +1,113 @@
+### Reference
+### http://www.cs.cmu.edu/~epxing/Class/10708-13/reading/Ch%204.pdf
+import itertools
+from operator import itemgetter
+from functools import reduce
+import numpy as np
+import sys
+
+def read_uai_file(filename):
+    """
+    Reads uai file and extract a dictionary describing the network.
+    """
+    uai_info = {}
+
+    with open(filename, "r") as file_:
+        lines = file_.readlines()
+        lines = [l.replace("\n", "") for l in lines]
+        lines = [l.replace("\t", " ") for l in lines]
+
+        ### Exclude empty lines
+        lines = [l for l in lines if l != '']
+
+        uai_info["network_type"] = lines[0]
+        uai_info["n_variables"] = int(lines[1])
+
+        ### Variables
+        uai_info["variables"] = {}
+        for i in range(uai_info["n_variables"]):
+            uai_info["variables"][i + 1] = {}
+
+            uai_info["cardinalities"] = [int(c) for c in lines[2].split(" ")]
+
+        ### Cliques
+        uai_info["n_cliques"] = int(lines[3])
+        uai_info["cliques"] = {}
+        for i in range(uai_info["n_cliques"]):
+            uai_info["cliques"][i] = {}
+
+            uai_info["cliques"][i]["vars"] = [int(c) for c in lines[3 + (i + 1)].split(" ")][1:]
+            uai_info["cliques"][i]["potential"] = [c for c in lines[3 + uai_info["n_cliques"] + (2 * i + 2)].split(" ")]
+
+    return uai_info
+
+def retrieve_potential(evidence, variables, markov_network):
+    """
+    Retrieve the potential from a certain evidence considering a certain potential table containing variables.
+    """
+
+    potential = 0
+    for c in range(markov_network["n_cliques"]):
+        if variables == markov_network["cliques"][c]["vars"]:
+            potential = markov_network["cliques"][c]["potential"]
+
+    all_possibilities = []
+    for i in range(len(variables)):
+        all_possibilities.append([k for k in range(markov_network["cardinalities"][variables[i]])])
+
+    all_possibilities = itertools.product(*all_possibilities)
+    all_possibilities = [a for a in all_possibilities]
+
+    pot = all_possibilities.index(evidence)
+    pot = potential[pot]
+
+    return float(pot)
+
+def markov_partition(markov_network):
+    """
+    CAlculates markov partition given a markov network described by a uai file.
+    """
+    evidences = []
+    for i in range(markov_network["n_variables"]):
+        evidences.append([k for k in range(markov_network["cardinalities"][i])])
+
+    evidences = itertools.product(*evidences)
+    evidences = [e for e in evidences]
+
+    result = 0
+    for evidence in evidences:
+        clique_potentials = []
+        for c in range(markov_network["n_cliques"]):
+            ### Check if the clique is maximal
+            ### If not, don't consider it
+            ### For now, it only excludes singletons
+            if len(markov_network["cliques"][c]["vars"]) == 1:
+                continue
+
+            ### Consider only variables present at the considered clique
+            reduced_evidence = itemgetter(*markov_network["cliques"][c]["vars"])(evidence)
+
+            ### When there's only one variable the itemgetter does not return a tuple
+            if isinstance(reduced_evidence, int): reduced_evidence = (reduced_evidence, )
+
+            ### Get the potential from all the cliques
+            clique_potentials.append(retrieve_potential(reduced_evidence, markov_network["cliques"][c]["vars"], markov_network))
+
+        #print(clique_potentials)
+        result += reduce(lambda x, y: x * y, clique_potentials)
+
+    return result
+
+
+
+filename_ = sys.argv[1]
+
+# markov_network = read_uai_file("grid3x3.uai")
+# markov_network = read_uai_file("problem.uai")
+# markov_network = read_uai_file("misconception.uai")
+
+markov_network = read_uai_file(filename_)
+partition = markov_partition(markov_network)
+
+print("Partition function: {}".format(partition))
+print("Log10: {}".format(np.log10(partition)))
